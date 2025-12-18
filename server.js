ec3a384d136d1c7fb3a50cd31e4a129cae2ef2fb
Baxtiyor Abduxakimov <105661906+abduxakimov07@users.noreply.github.com>
2025-12-18 20:25:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.static(__dirname)); // Bu qator papkadagi fayllarni brauzerga ochib beradi
 app.use(express.json({ limit: '50mb' })); // Katta sahifalarni sig'dirish uchun
 
 app.post('/upload-html', (req, res) => {
@@ -16,4 +17,5 @@ app.get('/latest', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server ${PORT}-portda ishlayapti`));
